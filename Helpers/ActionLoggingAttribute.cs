4c0b09d6deed1d002003983c4061c6c877221fb6
CGC\cbodley <cbodley@ssmic.com>
2018-01-03 11:02:23 -0500

    we have commited this test for log for net

---
+++ Helpers/ActionLoggingAttribute.cs
@@ -0,0 +1,43 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+
+
+namespace TestLog4Net.Helpers
+{
+    public class ActionLoggingAttribute : System.Web.Mvc.ActionFilterAttribute
+    {
+        protected log4net.ILog Log = log4net.LogManager.GetLogger(typeof(Controller));
+
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            if (filterContext.Exception != null)
+            {
+                log4net.Config.XmlConfigurator.Configure();
+                log4net.LogicalThreadContext.Properties["NetworkId"] = HttpContext.Current.User.Identity.Name;
+                log4net.LogicalThreadContext.Properties["Action"] = filterContext.RouteData.Values["Action"];
+                log4net.LogicalThreadContext.Properties["Controller"] = filterContext.RouteData.Values["Controller"];
+                Log.Error("Exception Has Happend", filterContext.Exception);
+            }
+            base.OnActionExecuted(filterContext);
+        }
+
+
+        [ValidateInput(false)]
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            base.OnActionExecuting(filterContext);
+            log4net.Config.XmlConfigurator.Configure();
+            log4net.LogicalThreadContext.Properties["NetworkId"] = HttpContext.Current.User.Identity.Name;
+            log4net.LogicalThreadContext.Properties["Action"] = filterContext.RouteData.Values["Action"];
+            log4net.LogicalThreadContext.Properties["Controller"] = filterContext.RouteData.Values["Controller"];
+
+            var actionParams = filterContext.ActionParameters;
+            var fullparams = filterContext.RequestContext.HttpContext.Request.Params;
+
+            log4net.LogicalThreadContext.Properties["Params"] =
+                JsonConvert.SerializeObject(new { action = actionParams, full = fullparams });
+            Log.Info("User Action");
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TestLog4Net")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard Company")]
+[assembly: AssemblyProduct("TestLog4Net")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("48b762d9-f09e-4fe5-b1f4-ee406757c675")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
